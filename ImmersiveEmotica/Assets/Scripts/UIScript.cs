f74905f640cba99db6b8644ec033ebacc9f5dc7f
Joshua Novak <novakjo@shernet.sheridancollege.ca>
2019-11-24 23:39:38 -0700

    Added menu UI and functionality

---
+++ ImmersiveEmotica/Assets/Scripts/UIScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartButton() {
+        SceneManager.LoadScene("SadScene");
+    }
+
+    public void QuitButton() {
+        Application.Quit(0);
+    }
+}
